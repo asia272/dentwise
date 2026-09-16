747cdf194a30830f2c07fb709455dc84a75b3043
asia272 <asiaashraf7272@gmail.com>
2026-09-16 12:43:26 +0500

    sync user successfully

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardPage = () => {
+    return (
+        <div>Well come to DashboardPage</div>
+    )
+}
+
+export default DashboardPage
--- app/page.tsx
+++ app/page.tsx
@@ -1,24 +1,33 @@
-import CTA from '@/components/landing/CTA'
-import Footer from '@/components/landing/Footer'
-import Header from '@/components/landing/Header'
-import Hero from '@/components/landing/Hero'
-import HowItWork from '@/components/landing/HowItWork'
-import PricingSection from '@/components/landing/PricingSection'
-import WhatToAsk from '@/components/landing/WhatToAsk'
-import React from 'react'
+import CTA from "@/components/landing/CTA";
+import Footer from "@/components/landing/Footer";
+import Header from "@/components/landing/Header";
+import Hero from "@/components/landing/Hero";
+import HowItWorks from "@/components/landing/HowItWork";
 
-const page = () => {
+import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
+import { syncUser } from "@/lib/actions/users";
+import { currentUser } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function Home() {
+  const user = await currentUser();
+
+  // the best way of syncing => webhooks
+  await syncUser();
+
+  // redirect auth user to dashboard
+  if (user) redirect("/dashboard");
+
   return (
     <div className="min-h-screen bg-background">
       <Header />
       <Hero />
-      <HowItWork />
+      <HowItWorks />
       <WhatToAsk />
       <PricingSection />
       <CTA />
       <Footer />
     </div>
-  )
+  );
 }
-
-export default page
